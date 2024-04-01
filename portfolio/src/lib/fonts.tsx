5b3b819419b68224651d041e9ed0bee91c6775ea
Naoya Minomura <naotoitoi@gmail.com>
2024-04-01 22:39:00 +0900

    ChakraUI・フォントの設定

---
+++ portfolio/src/lib/fonts.tsx
@@ -0,0 +1,10 @@
+import { Zen_Maru_Gothic } from 'next/font/google'
+
+const zen = Zen_Maru_Gothic({
+    weight: "400",
+    subsets: ["latin"],
+})
+
+export const fonts = {
+    zen,
+}
--- portfolio/src/pages/_app.tsx
+++ portfolio/src/pages/_app.tsx
@@ -1,5 +1,27 @@
+import { fonts } from '@/lib/fonts';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import type { AppProps } from "next/app";
 
+export const theme = extendTheme({
+  fonts: {
+    heading: 'var(--font-zen)',
+    body: 'var(--font-zen)',
+  }
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>
+        {`
+        :root {
+          --font-zen: ${fonts.zen.style.fontFamily}
+        }
+      `}
+      </style>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  )
 }
